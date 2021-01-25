5d5be57239222a1f1e8f73ac2fa13f22a3646388
Tony Hutson <thutson@salesforce.com>
2021-01-25 10:37:34 -0500

    add function and input/answer in main

--- test.c
+++ test.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+int add(int x, int y){
+	return x+y;
+}
+
 int main(void){
 	printf("Hellow world!\n");
+	int num1, num2;
+	num1=0;
+	num2=0;
+	printf("\nPlease enter num1:");
+	scanf("%d",&num1);
+	printf("\nPlease enter num2:");
+	scanf("%d",&num2);
+	printf("\n\n %d + %d = %d\n", num1, num2, add(num1,num2));
+
 	return 0;
 }
 
